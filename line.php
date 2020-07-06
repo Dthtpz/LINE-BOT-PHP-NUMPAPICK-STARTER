9aae5e328b68f9c450cf6129dbfaf6b64b2b4778
Dthtpz <60869031+Dthtpz@users.noreply.github.com>
2020-07-06 21:40:09 +0700

    Update line.php

--- line.php
+++ line.php
@@ -2,7 +2,7 @@
   
 
 function send_LINE($msg){
- $access_token = 'qLip9omRdSnsaKFlsWmCCx9pdvAcRd1CGb6XfH/K3aKVgmHS4Eh/a35I8S1q8XVCZQVJUVIPa2B/c1ZJHfEyA8vUgqlUeIfqTkw607IKQ7yCasUHW34wj+CGzB6bOafYNDSGkh87GIr+Tns7fqFqVAdB04t89/1O/w1cDnyilFU='; 
+ $access_token = 'ddEV77o8kdwAMupkgssFq+szs6dwZH8qV7LmT41hFxkqXc83J+lJF/ZJVMxG502jUwLTn/JhObdBJNSLb2IOwNeT9K5syYYN/gmdqXPE1IbYpt5mAfjFieZ57deh+vqNc0Vu5OqrldfxRcNIiK5TZQdB04t89/1O/w1cDnyilFU='; 
 
   $messages = [
         'type' => 'text',
@@ -14,7 +14,7 @@ function send_LINE($msg){
       $url = 'https://api.line.me/v2/bot/message/push';
       $data = [
 
-        'to' => 'Ue77a191627f6ac91899e75d92264310c',
+        'to' => 'U18335f95c7f7feccd8a40ea755ad778a',
         'messages' => [$messages],
       ];
       $post = json_encode($data);
